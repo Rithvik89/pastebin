578b0bd0c42bcd806b431e70b8ddacde9ec29136
rithvik <innovation6178@gmail.com>
2023-04-30 18:10:12 +0530

    few more fields in App got added

--- http/main.go
+++ http/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"pastebin/internal/read_service"
+	"pastebin/internal/write_service"
 )
 
 type App struct {
-	srv *http.Server
+	srv           *http.Server
+	DB            *sql.DB
+	write_service *write_service.WriteService
+	read_service  *read_service.ReadService
 }
 
 func main() {
 
 	app := &App{}
 	initServer(app)
+	initDB(app)
 
 	fmt.Println("Initialized the services required for this web server")
 
