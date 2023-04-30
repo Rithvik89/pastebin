92e07384d3466723decf22665bf87ff8818883f6
rithvik <innovation6178@gmail.com>
2023-04-30 18:09:32 +0530

    init Services and initDB added

--- http/init.go
+++ http/init.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+	"pastebin/internal/read_service"
+	"pastebin/internal/write_service"
+	"pastebin/sqlc_db"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func initServer(app *App) {
 
+	r := chi.NewRouter()
+
+	initHandler(r)
+
 	srv := http.Server{
-		Addr:    "localhost:3000",
-		Handler: nil,
+		Addr:    "localhost:5001",
+		Handler: r,
 	}
 
 	app.srv = &srv
 
+}
+
+func initDB(app *App) {
+	db, err := sql.Open("mysql", "root:mypassword@/mysql_pb")
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.DB = db
+}
+
+func initServices(app *App) {
+	querier := sqlc_db.New(app.DB)
+	app.write_service = write_service.New(querier)
+	app.read_service = read_service.New(querier)
 }
